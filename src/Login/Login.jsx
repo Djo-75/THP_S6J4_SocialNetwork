cb1779798f0f7975e9512407c131c9bcc9a1e327
Djo-75 <j.pecquet@live.fr>
2023-05-18 15:53:16 +0200

    page login et profil okay!

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
 const Login = () => {
+    const [identifier, setIdentifier] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleIdentifierChange = (e) => {
+        setIdentifier(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+        // Here you can implement your authentication logic
+        // For simplicity, let's just log the entered Identifier and password
+        const data = {
+            identifier: identifier,
+            password: password
+        };
+
+        fetch('http://localhost:1337/api/auth/local/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                const token = responseData.jwt;
+                console.log(responseData.jwt)
+                Cookies.set("token", token);
+            });
+
+
+    };
+
     return (
         <div>
             <h2>Login</h2>
+            <form>
+                <label>
+                    Identifier:
+                    <input type="text" name={identifier} onChange={handleIdentifierChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" name={password} onChange={handlePasswordChange} />
+                </label>
+                <br />
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
         </div>
     );
 };
--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 const Profile = () => {
+
+    const [monprofil, setMonProfil] = useState("Pas de profil")
+
+
+    useEffect(() => {
+        if (Cookies.get("token")) {
+            fetch("http://localhost:1337/api/users/me", {
+                method: "get",
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("token")}`,
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((responseData) => {
+                    setMonProfil(`email : ${responseData.email}
+                            id : ${responseData.id}
+                            username : ${responseData.username}
+                            créé le : ${responseData.createdAt} `);
+                });
+        } else {
+            setMonprofil(`Vous n'êtes pas connecté. Vous n'avez donc pas de profil`);
+        }
+    }, [])
+
     return (
         <div>
             <div>
                 <h2>Profil user</h2>
+                <div>{monprofil}</div>
             </div>
         </div>
     );
